0629b828ca0cbc8e40630310c58620d282eac0b6
TRUSTTEAM\mmylle <mmylle@trustteam.be>
2023-04-21 21:03:17 +0200

    another one?---

--- src/api.js
+++ src/api.js
@@ -1,21 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+/*
 const express = require("express");
 const serverless = require("serverless-http");
 
 const app = express();
-//const router = express.Router();
+const router = express.Router();
 
 
-app.use(express.static('public'))
 
-/*
 router.get("/", (req, res) => {
   res.json({
     hello: "hi!"
   });
 });
-*/
 
-//app.use(`/.netlify/functions/api`, router);
+app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
+*/
