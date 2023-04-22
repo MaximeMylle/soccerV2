b19caddae6a146f1ae48a071b7db63dc7f5097a0
TRUSTTEAM\mmylle <mmylle@trustteam.be>
2023-04-22 16:09:10 +0200

    updated some data in local db

--- src/api.js
+++ src/api.js
@@ -14,13 +14,38 @@ const users = [
 
 const teams = [
   {
+    id: "0",
+    Name: "NO GAME",
+    Location: "NONE"
+  },
+  {
     id: "1",
     Name: "Sport na Arbeid",
     Location: "Veldegem"
   },
   {
     id: "2",
-    Name: "KroonHove",
+    Name: "VV KroonHove",
+    Location: "Unknown"
+  },
+  {
+    id: "3",
+    Name: "FC Veteranen t'Oost",
+    Location: "Unknown"
+  },
+  {
+    id: "4",
+    Name: "Sportgroep 70",
+    Location: "Unknown"
+  },
+  {
+    id: "5",
+    Name: "Oud Savio",
+    Location: "Unknown"
+  },
+  {
+    id: "6",
+    Name: "Oksa",
     Location: "Unknown"
   }
 ]
@@ -28,16 +53,30 @@ const teams = [
 const games = [
   {
     id: "1",
-    Home_Team_Id: "1",
-    Out_Team_Id: "2",
-    Date: "01/01/2023",
+    Home_Team_Id: "2",
+    Out_Team_Id: "1",
+    Date: "03/09/2023",
     score: "-"
   },
   {
     id: "2",
     Home_Team_Id: "1",
-    Out_Team_Id: "2",
-    Date: "01/01/2023",
+    Out_Team_Id: "3",
+    Date: "10/09/2023",
+    score: "-"
+  },
+  {
+    id: "3",
+    Home_Team_Id: "0",
+    Out_Team_Id: "0",
+    Date: "17/09/2023",
+    score: "-"
+  },
+  {
+    id: "4",
+    Home_Team_Id: "1",
+    Out_Team_Id: "4",
+    Date: "17/09/2023",
     score: "-"
   }
 ]
