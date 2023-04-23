1911a573c4a593dd2be4c0e0bb43cdec7ae59326
TRUSTTEAM\mmylle <mmylle@trustteam.be>
2023-04-23 09:58:51 +0200

    insert some actual data & games

--- dist/script.js
+++ dist/script.js
@@ -28,7 +28,9 @@ var generateHomeInfo = async function(){
         rowToAdd +="<td>" + game.Date + "</td>"
         rowToAdd +="<td>" + teams.find(x => x.id === game.Home_Team_Id).Name + "</td>"
         rowToAdd +="<td>" + teams.find(x => x.id === game.Out_Team_Id).Name + "</td>"
+        rowToAdd +="<td>" + game.Score + "</td>"
         rowToAdd +="<td>" + teams.find(x => x.id === game.Home_Team_Id).Location + "</td>"
+        rowToAdd +="<td>" +"<a href='gameDetails.html?gameId="+game.ID+"'>Game Details</a>"+ "</td>"
         rowToAdd += "</tr>"
       });
 
--- src/api.js
+++ src/api.js
@@ -19,33 +19,63 @@ const teams = [
     Location: "NONE"
   },
   {
-    id: "1",
+    id: "12",
     Name: "Sport na Arbeid",
-    Location: "Veldegem"
+    Location: "Acaciastraat 31b, 8210 Zedelgem"
   },
   {
-    id: "2",
+    id: "11",
     Name: "VV KroonHove",
     Location: "Unknown"
   },
   {
-    id: "3",
+    id: "5",
     Name: "FC Veteranen t'Oost",
     Location: "Unknown"
   },
   {
-    id: "4",
+    id: "3",
     Name: "Sportgroep 70",
     Location: "Unknown"
   },
   {
-    id: "5",
+    id: "1",
     Name: "Oud Savio",
     Location: "Unknown"
   },
   {
-    id: "6",
+    id: "2",
     Name: "Oksa",
+    Location: "Unknown"
+  },
+  {  
+    id: "4",
+    Name: "VK Vogelzang",
+    Location: "Unknown"
+  },
+  {  
+    id: "8",
+    Name: "TC Oedelem",
+    Location: "Unknown"
+  },
+  {  
+    id: "7",
+    Name: "De Rickvrienden",
+    Location: "Unknown"
+  },
+  {  
+    id: "14",
+    Name: "De Brugse Vrienden",
+    Location: "Unknown"
+  },
+  {  
+    id: "6",
+    Name: "St. Joris Vrienden",
+    Location: "Unknown"
+  },
+  {  
+    id: "12",
+    Name: "BammBamm",
     Location: "Unknown"
   }
 ]
@@ -53,15 +83,15 @@ const teams = [
 const games = [
   {
     id: "1",
-    Home_Team_Id: "2",
-    Out_Team_Id: "1",
+    Home_Team_Id: "11",
+    Out_Team_Id: "12",
     Date: "03/09/2023",
     score: "-"
   },
   {
     id: "2",
-    Home_Team_Id: "1",
-    Out_Team_Id: "3",
+    Home_Team_Id: "12",
+    Out_Team_Id: "5",
     Date: "10/09/2023",
     score: "-"
   },
@@ -74,9 +104,93 @@ const games = [
   },
   {
     id: "4",
+    Home_Team_Id: "12",
+    Out_Team_Id: "3",
+    Date: "24/09/2023",
+    score: "-"
+  },
+  {
+    id: "5",
     Home_Team_Id: "1",
-    Out_Team_Id: "4",
-    Date: "17/09/2023",
+    Out_Team_Id: "12",
+    Date: "01/10/2023",
+    score: "-"
+  },
+  {
+    id: "6",
+    Home_Team_Id: "12",
+    Out_Team_Id: "2",
+    Date: "08/10/2023",
+    score: "-"
+  },
+  {
+    id: "7",
+    Home_Team_Id: "4",
+    Out_Team_Id: "12",
+    Date: "15/10/2023",
+    score: "-"
+  },
+  {
+    id: "8",
+    Home_Team_Id: "12",
+    Out_Team_Id: "8",
+    Date: "22/10/2023",
+    score: "-"
+  },
+  {
+    id: "9",
+    Home_Team_Id: "3",
+    Out_Team_Id: "12",
+    Date: "29/10/2023",
+    score: "-"
+  },
+  {
+    id: "10",
+    Home_Team_Id: "12",
+    Out_Team_Id: "7",
+    Date: "05/11/2023",
+    score: "-"
+  },
+  {
+    id: "11",
+    Home_Team_Id: "14",
+    Out_Team_Id: "12",
+    Date: "12/11/2023",
+    score: "-"
+  },
+  {
+    id: "12",
+    Home_Team_Id: "12",
+    Out_Team_Id: "6",
+    Date: "19/11/2023",
+    score: "-"
+  },
+  {
+    id: "13",
+    Home_Team_Id: "12",
+    Out_Team_Id: "10",
+    Date: "26/11/2023",
+    score: "-"
+  },
+  {
+    id: "14",
+    Home_Team_Id: "0",
+    Out_Team_Id: "0",
+    Date: "03/12/2023",
+    score: "-"
+  },
+  {
+    id: "15",
+    Home_Team_Id: "3",
+    Out_Team_Id: "12",
+    Date: "10/12/2023",
+    score: "-"
+  },
+  {
+    id: "16",
+    Home_Team_Id: "12",
+    Out_Team_Id: "1",
+    Date: "17/12/2023",
     score: "-"
   }
 ]
